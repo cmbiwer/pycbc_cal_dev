c173787cc4c3d7d4c0c2213a0141c1b65b089e2a
Christopher.Biwer <christopher.biwer@ligo.org>
2015-05-11 12:46:38 -0500

    The cal example script now reads and applies a filter.

--- examples/cal/calibrate_data/run_calibrate_data.sh
+++ examples/cal/calibrate_data/run_calibrate_data.sh
@@ -2,9 +2,9 @@
 
 # test analysis
 GPS_START_TIME=1102660000
-GPS_END_TIME=1102662000
+GPS_END_TIME=1102660200
 IFO=L
-FRAME_TYPE=L1_R
+FRAME_TYPE=L1_RDS
 FRAME_CACHE=frame_caches/L1-DATAFIND-${GPS_START_TIME}-$((${GPS_END_TIME}-${GPS_START_TIME})).lcf
 
 # run the datafind script to create a frame cache that contains
