f03d47ef6945e33cdc557272c5328b18bbd0bc08
Christopher.Biwer <christopher.biwer@ligo.org>
2015-05-11 11:48:36 -0500

    Added example of running datafind for cal example.

---
+++ examples/cal/calibrate_data/run_datafind.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# test analysis
+GPS_START_TIME=1102660000
+GPS_END_TIME=1102662000
+IFO=L
+FRAME_TYPE=L1_R
+
+# run the datafind script to create a frame cache that contains
+# the path of the frame files we requested
+ligo_data_find --lal-cache --observatory ${IFO} --type ${FRAME_TYPE} --gps-start-time $GPS_START_TIME --gps-end-time $GPS_END_TIME --url-type file > L1-DATAFIND-${GPS_START_TIME}-${DURATION}.lcf
