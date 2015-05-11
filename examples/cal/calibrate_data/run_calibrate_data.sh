56f36595b1a520b188ac08d7d37f342b194b4e45
Christopher.Biwer <christopher.biwer@ligo.org>
2015-05-11 11:52:15 -0500

    Added running calibrate data in the bash script for the cal example.

---
+++ examples/cal/calibrate_data/run_calibrate_data.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# test analysis
+GPS_START_TIME=1102660000
+GPS_END_TIME=1102662000
+IFO=L
+FRAME_TYPE=L1_R
+FRAME_CACHE=frame_caches/L1-DATAFIND-${GPS_START_TIME}-$((${GPS_END_TIME}-${GPS_START_TIME})).lcf
+
+# run the datafind script to create a frame cache that contains
+# the path of the frame files we requested
+ligo_data_find --lal-cache --observatory ${IFO} --type ${FRAME_TYPE} --gps-start-time $GPS_START_TIME --gps-end-time $GPS_END_TIME --url-type file > ${FRAME_CACHE}
+
+# calibrate data
+python pycbc_calibrate_data --gps-start-time ${GPS_START_TIME} --gps-end-time ${GPS_END_TIME} --frame-cache ${FRAME_CACHE}
