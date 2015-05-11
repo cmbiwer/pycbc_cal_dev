a3da068fd8e85e6b7be7c76bbb7eba7ed3120ad6
Christopher.Biwer <christopher.biwer@ligo.org>
2015-05-11 14:23:00 -0500

    Removed the filter filter reading example for the cal examples dir.

--- examples/cal/read_filter_file/read_filter_file.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-
-import ROOT
-ROOT.gSystem.Load("/usr/lib64/libdmtsigp.so")
-ROOT.gSystem.Load("/usr/lib64/libgdsplot.so")
-from foton import FilterFile
-
-# read the foton filter file
-filter_file = FilterFile('L1OAF.txt')
