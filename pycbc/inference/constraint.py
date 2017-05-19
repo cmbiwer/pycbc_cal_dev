2f93ce7e95e65c0574dd4e8fcc1982659a65b104
Christopher M. Biwer <christopher.biwer@ligo.org>
2017-05-19 19:08:38 -0400

    Add docstrings.

--- pycbc/inference/constraint.py
+++ pycbc/inference/constraint.py
@@ -3,6 +3,9 @@ from pycbc.distributions import bounded
 from pycbc.workflow import configuration
 
 class Constraint(object):
+    """ Creates a constraint that evaluates to True if parameters obey
+    the constraint and False if they do not.
+    """
     name = "custom"
     required_name = []
     def __init__(self, variable_args, **kwargs):
@@ -10,22 +13,30 @@ class Constraint(object):
             setattr(self, kwarg, kwargs[kwarg])
         _, self.transforms = transforms.get_conversions(
                                        self.required_parameters, variable_args)
-        print self.transforms, self.required_parameters
 
     def __call__(self, params):
+        """ Evaluates constraint.
+        """
         params = transforms.apply_conversions(params, self.transforms) \
                      if self.transforms else params
         return self._constraint(params)
 
     def _constraint(self, params):
+        """ Evaluates constraint function.
+        """
         return eval(self.func)
 
 class MtotalLT(Constraint):
+    """ Pre-defined constraint that check if total mass is less than a value.
+    """
     name = "mtotal_lt"
     required_parameters = ["mass1", "mass2"]
     def _constraint(self, params):
+        """ Evaluates constraint function.
+        """
         return params["mass1"] + params["mass2"] < self.mtotal
 
+# list of all constraints
 constraints = {
     Constraint.name : Constraint,
     MtotalLT.name : MtotalLT,
